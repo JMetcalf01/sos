ad05cdb3a0befd3cfb46360ca8ece69e34d50a00
JonathanMetcalf <jonathan.e.metcalf@gmail.com>
2020-01-18 18:44:28 -0500

    Working compiling for setting variables, function calls

--- sosc/src/main/kotlin/Parser.kt
+++ sosc/src/main/kotlin/Parser.kt
@@ -21,16 +21,21 @@ class Parser constructor(private val tokens: List<Token>, private val outputPath
         println(tokens)
 
         while (index < tokens.size) {
-            // If it's a function, check the syntax
-            if (tokens[index].tokenType == TokenType.FUNC) {
-                funcNameChecker()
-                funcBodyChecker()
+
+            when (tokens[index].tokenType) {
+                TokenType.FUNC -> {
+                    funcNameChecker()
+                    funcBodyChecker()
+                    text += "\n"
+                }
+
             }
 
             index++
         }
 
         // Writes text to the file and closes
+        text += "\n"
         writer.write(text)
         writer.close()
     }
@@ -55,9 +60,7 @@ class Parser constructor(private val tokens: List<Token>, private val outputPath
 
             // Does parameters
             index++
-            for (param in findParams(index)) {
-                text += " $param"
-            }
+            findFuncParams()
         }
 
         // Moves index to next line
@@ -72,27 +75,153 @@ class Parser constructor(private val tokens: List<Token>, private val outputPath
      *
      * @author Jonathan Metcalf
      *
-     * @param start starting index of the tokens to look through (inclusive)
      * @return the list of the params
      */
-    private fun findParams(start: Int): List<String> {
-        val list = mutableListOf<String>()
-        var currentIndex = start
-        while (tokens[currentIndex].tokenType != TokenType.RIGHT_PARENTHESES) {
+    private fun findFuncParams() {
+        while (tokens[index].tokenType != TokenType.RIGHT_PARENTHESES) {
             // If it's unknown, it's a parameter to add
-            if (tokens[currentIndex].tokenType == TokenType.UNKNOWN)
-                list.add(tokens[currentIndex].unicode)
+            if (tokens[index].tokenType == TokenType.UNKNOWN)
+                text += " ${tokens[index].unicode}"
+
+            // Finding string
+            if (tokens[index].tokenType == TokenType.QUOTE_MARK)
+                getString()
 
             // Parentheses are incorrect if this is true
-            if (tokens[currentIndex].tokenType == TokenType.BRACE || tokens[currentIndex].tokenType == TokenType.NEW_LINE)
-                throw RuntimeException("Expected ${TokenType.RIGHT_PARENTHESES} but found a ${tokens[currentIndex].tokenType} instead!")
-            currentIndex++
-        }
+            if (tokens[index].tokenType == TokenType.BRACE || tokens[index].tokenType == TokenType.NEW_LINE)
+                throw RuntimeException("Expected ${TokenType.RIGHT_PARENTHESES} but found a ${tokens[index].tokenType} instead!")
 
-        return list
+            index++
+        }
     }
 
+    /**
+     * Reads the body of a function and parses it.
+     *
+     * @author Jonathan Metcalf
+     */
     private fun funcBodyChecker() {
+        val start = index
+        when (tokens[index].tokenType) {
+            TokenType.UNKNOWN -> {
+                index++
+                findCalledParams()
+                text += "call ${tokens[start].unicode}\n"
+            }
 
+            TokenType.VAR -> {
+                storeVariable()
+            }
+
+            TokenType.IF -> {
+
+            }
+
+            TokenType.ELSE -> {
+
+            }
+
+            TokenType.WHILE -> {
+
+            }
+        }
+    }
+
+    /**
+     * Returns a string from the starting quotemark to
+     * the next quotemark.
+     *
+     * @return the string (minus the quotemarks)
+     */
+    private fun getString() {
+        index++
+        val start = index
+        while (tokens[index].tokenType != TokenType.QUOTE_MARK) {
+            index++
+        }
+
+        for (i in start until index) {
+            text += tokens[i].unicode
+        }
+
+        index++
+    }
+
+    //find all params, figure out if variable or literal (loadr if literal, load if variable)
+    private fun findCalledParams() {
+        while (tokens[index].tokenType != TokenType.RIGHT_PARENTHESES) {
+            // If it's unknown, it's a parameter to add
+            if (tokens[index].tokenType == TokenType.UNKNOWN) {
+                text += "load ${tokens[index].unicode}\n"
+
+            }
+
+            // Finding string
+            if (tokens[index].tokenType == TokenType.QUOTE_MARK) {
+                index++
+                text += "loadr "
+                getString()
+                text += "\n"
+            }
+
+            // Parentheses are incorrect if this is true
+            if (tokens[index].tokenType == TokenType.BRACE || tokens[index].tokenType == TokenType.NEW_LINE)
+                throw RuntimeException("Expected ${TokenType.RIGHT_PARENTHESES} but found a ${tokens[index].tokenType} instead!")
+
+            index++
+        }
+    }
+
+    private fun storeVariable() {
+        index++
+        if (tokens[index].tokenType != TokenType.SPACE)
+            throw RuntimeException("Expected ${TokenType.SPACE} but found ${tokens[index].tokenType}!")
+
+        index++
+        text += "store ${tokens[index].unicode} "
+        index++
+        findValue()
+    }
+
+    private fun findValue() {
+        while (tokens[index].tokenType == TokenType.SPACE) {
+            index++
+        }
+
+        if (tokens[index].tokenType != TokenType.EQUALS)
+            throw RuntimeException("Expected ${TokenType.EQUALS} but found ${tokens[index].tokenType}!")
+
+        index++
+        while (tokens[index].tokenType == TokenType.SPACE) {
+            index++
+        }
+
+        while (tokens[index].tokenType != TokenType.NEW_LINE) {
+            if (tokens[index].tokenType == TokenType.QUOTE_MARK)
+                getString()
+            else if (tokens[index].tokenType != TokenType.SPACE) {
+                text += getStringNumber(tokens[index])
+
+            }
+            index++
+        }
+
+        text += "\n"
+    }
+
+    private fun getStringNumber(token: Token): String {
+        return when (token.tokenType) {
+            TokenType.ONE -> "1"
+            TokenType.TWO -> "2"
+            TokenType.THREE -> "3"
+            TokenType.FOUR -> "4"
+            TokenType.FIVE -> "5"
+            TokenType.SIX -> "6"
+            TokenType.SEVEN -> "7"
+            TokenType.EIGHT -> "8"
+            TokenType.NINE -> "9"
+            TokenType.ZERO -> "0"
+            else -> throw IllegalStateException("getStringNumber called on non-number!")
+        }
     }
 }
