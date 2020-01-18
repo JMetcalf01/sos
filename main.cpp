010c406c3697ca7046835e3276530310e2d27abd
Troy Mullenberg <tmullenberg@hotmail.com>
2020-01-18 11:17:33 -0500

    Debugged most of the file IO to the point of creating the functions TODO: figure out how to get local files (txt files) to load in without full local paths

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <string>
 #include "variablestack.h"
 #include "function.h"
 
@@ -8,46 +9,64 @@ using namespace sos;
 
 
 void testFunction();
-std::vector<std::string> readFile(std::string &fileName);
-
+std::vector<std::string> readFile();
+void parseForFunction(std::vector<std::string> lines);
 
 
 /**
  *  Makes a map of the functions (They are added by the file parser)
  */
 std::map<std::string, Function>  functions;
-std::vector<std::string> lines;
-
 std::map<std::string, void (*) ()>  instructions;
 //operation.insert(std::pair<std::string, void (*) ()>("This is a test", testFunction));
 int main() {
+    std::vector<std::string> lines;
+
     std::cout << "Hello, World!" << std::endl;
-    lines = readFile((std::string &) "text.txt");
+    lines  = readFile();
+    std::cout << "testing" << std::endl;
+    std::cout << lines.at(0) << std::endl;
+    parseForFunction(lines);
     return 0;
 }
 void parseForFunction(std::vector<std::string> lines)
 {
+    std::vector<std::string> params;
     std::string str;
     int i = 0;
     for ( std::string str : lines) {
         int startIndex = 0;
-        std::vector<std::string> params;
-        std::vector<std::string> subParams(params.begin() + 2, params.end());
 
         if (str.find("func") == 0)
         {
             startIndex = i;
-            std::string token = str.substr(0, str.find(" "));
-            while ((str.find(" ")) != std::string::npos)
+//            std::string token = str.substr(0, str.find(" "));
+//            while ((str.find(" ")) != std::string::npos)
+//            {
+//                int pos = str.find(" ");
+//                std::string token = str.substr(0, pos);
+//                params.push_back(token);
+//            }
+            std::string word = "";
+            for (auto x : str)
             {
-                int pos = str.find(" ");
-                token = str.substr(0, pos);
-                params.push_back(token);
+                if (x == ' ')
+                {
+                    std::cout << word << std::endl;
+                    params.push_back(word);
+                    word = "";
+                }
+                else
+                {
+                    word = word + x;
+                }
             }
+            std::cout << word << std::endl;
         }
         if (str.empty())
         {
-            functions.insert(std::pair<std::string, Function>(params.at(1), Function(&instructions, params.at(1), subParams, lines, startIndex, i)));
+            std::vector<std::string> subParams(params.begin() + 1, params.end());
+            functions.insert(std::pair<std::string, Function>(params.at(1), Function(&instructions, params.at(1), subParams, lines, startIndex, i + 1)));
         }
         i++;
     }
@@ -57,14 +76,16 @@ void testFunction() {
     std::cout << "This is a test function" << std::endl;
 }
 
-std::vector<std::string> readFile(std::string &fileName)
+std::vector<std::string>  readFile()
 {
     std::vector<std::string> linesImport;
-    std::ifstream file(fileName);
 
-    for( std::string line; getline( file, line ); )
+    std::ifstream file("ToDebug...");
+    std::string str;
+    while (std::getline(file, str))
     {
-        linesImport.push_back(line);
+        std::cout << str << "\n";
+        linesImport.push_back(str);
     }
     return linesImport;
 }
