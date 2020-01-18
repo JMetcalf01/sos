f81458a3f5593869a819735656384fc6a5220e7b
JonathanMetcalf <jonathan.e.metcalf@gmail.com>
2020-01-17 19:56:48 -0500

    Creates list of characters in Main, changed Emoji to Token,

--- sosc/src/main/kotlin/Main.kt
+++ sosc/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import java.io.*
 
 /**
  * The entry point into the program. The first argument should be
@@ -8,7 +9,7 @@
  * @param args the arguments to the function
  */
 fun main(args: Array<String>) {
-    Parser(args[0]).parse()
+    Parser(args[0], args[1]).parse()
 }
 
 /**
@@ -16,10 +17,33 @@ fun main(args: Array<String>) {
  *
  * @author Jonathan Metcalf and Martin Bleakley
  *
- * @property path the path of the file to be compiled
+ * @property inputPath the path of the file to be compiled
+ * @property outputPath the path of the compiled file
  */
-class Parser constructor(private val path: String) {
+class Parser constructor(private val inputPath: String, private val outputPath: String) {
+
+    private val reader: BufferedReader = BufferedReader(FileReader(inputPath))
+    private val writer: BufferedWriter = BufferedWriter(FileWriter(outputPath))
+    private val regex: Regex = Regex("[ ]+")
+
+    /**
+     * Parses through every line of the program to be compiled.
+     *
+     * @author Jonathan Metcalf and Martin Bleakley
+     *
+     */
     fun parse() {
-        print(Emojis.ONE)
+        // Convert to characters
+        var file = ""
+        reader.lines().forEach { file += "$it\n" }
+        val characters: MutableList<String> = regex.replace(file.replace("\n", " \\n "), " ").split(" ") as MutableList<String>
+
+        val tokens = mutableListOf<Token>()
+
+        testSyntax(tokens)
+    }
+
+    private fun testSyntax(tokens: List<Token>) {
+
     }
 }
